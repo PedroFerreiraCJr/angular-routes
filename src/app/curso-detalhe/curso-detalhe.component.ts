abb81e9c2630ee2b5e8e9fa1684e0a41c94a128d
Pedro <pedroferreiracjr@gmail.com>
2021-09-14 19:55:28 -0300

    aula sobre a escuta dos parâmetros da rota ativa

--- src/app/curso-detalhe/curso-detalhe.component.ts
+++ src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
@@ -9,16 +10,28 @@ import { ActivatedRoute } from '@angular/router';
 export class CursoDetalheComponent implements OnInit {
 
   id!: string;
+  subscription!: Subscription;
 
   // para pegar os valores da rota ativa, é preciso injetar uma instância de ActivatedRoute
   constructor(private route: ActivatedRoute) {
-    console.log(this.route);
+    //console.log(this.route);
     // esta é a primeira forma de pegar os valores dos parâmetros da rota ativa
     // lembrando que os parâmetros devem ser declarados para que possam ser recebidos através
     //deste objeto.
-    this.id = this.route.snapshot.params['id'];
+    // esta forma de capturar os parâmetros de rota foi comentado pois é suscetível a erros
+    //na observação dos parâmetros da rota ativa.
+    //this.id = this.route.snapshot.params['id'];
   }
 
   ngOnInit(): void {
+    // esta é a forma mais aconselhada de escutar os parâmetros de uma rota
+    this.subscription = this.route.params.subscribe((params: any) => {
+      this.id = params['id'];
+    });
+  }
+
+  ngOnDestroy(): void {
+    // quando o objeto for destruído, remove a escuta do objeto de rota
+    this.subscription?.unsubscribe();
   }
 }
