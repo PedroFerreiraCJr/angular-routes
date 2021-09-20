09737572a644c7b68b86b44f34898c9c50c446bf
Pedro <pedroferreiracjr@gmail.com>
2021-09-20 14:57:36 -0300

    reorganização do módulo de cursos do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,27 @@
-import { AppRoutingModule } from './app.routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
-import { CursosService } from './cursos/cursos.service';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    CursosModule,
     AppRoutingModule,
     NgbModule
   ],
-  providers: [CursosService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,15 +4,15 @@ import { ModuleWithProviders } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { CursosComponent } from './cursos/cursos.component';
-import { CursoDetalheComponent } from "./curso-detalhe/curso-detalhe.component";
+import { CursoDetalheComponent } from "./cursos/curso-detalhe/curso-detalhe.component";
 import { LoginComponent } from './login/login.component';
-import { CursoNaoEncontradoComponent } from "./curso-nao-encontrado/curso-nao-encontrado.component";
+import { CursoNaoEncontradoComponent } from "./cursos/curso-nao-encontrado/curso-nao-encontrado.component";
 import { HomeComponent } from "./home/home.component";
 
 // Esta palavra já era reservada do JavaScript mas não estava em uso,
 //agora no ES6 ela passou a ser utilizada, e significa ser final, semelhante ao
 //final do Java.
-const appRoutes: Routes = [    // Configuração de geral de rotas da aplicação
+const appRoutes: Routes = [    // Configuração geral de rotas da aplicação
   { path: 'cursos', component: CursosComponent },
   { path: 'curso/:id', component: CursoDetalheComponent }, // nesta rota foi definido um parâmetro chamado 'id'
   { path: 'login', component: LoginComponent },
@@ -26,6 +26,4 @@ const appRoutes: Routes = [    // Configuração de geral de rotas da aplicaçã
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule { }
