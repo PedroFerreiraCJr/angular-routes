ea56a9c824f53a94cc3443905a0c148bcec673f7
Pedro <pedroferreiracjr@gmail.com>
2021-09-20 14:28:37 -0300

    aula sobre adicionado e passando parâmetros de query para uma rota

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Subscription } from 'rxjs';
 
 import { CursosService } from './cursos.service';
 
@@ -7,13 +10,34 @@ import { CursosService } from './cursos.service';
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
-export class CursosComponent implements OnInit {
+export class CursosComponent implements OnInit, OnDestroy {
 
   cursos!: any[];
+  pagina!: number;
+  subscription!: Subscription;
 
-  constructor(private cursosService: CursosService) { }
+  constructor(
+    private cursosService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.cursos = this.cursosService.getCursos();
+    this.subscription = this.route.queryParams.subscribe((params) => {
+      this.pagina = params['pagina'];
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  public proximaPagina(): void {
+    this.pagina++;
+    // a passagem de parâmetros de query para a navegação programática
+    //é através de um objeto de configuração, como neste exemplo.
+    // o atributo queryParams é obrigatório, o atributo página é o valor passado
+    // para a rota.
+    this.router.navigate(['/cursos'], { queryParams: { pagina: this.pagina } });
   }
 }
