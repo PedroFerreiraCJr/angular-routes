bfe09465c9b712a5e41565fbe24e86a32fa7f971
Pedro <pedroferreiracjr@gmail.com>
2021-09-23 18:44:25 -0300

    aula sobre a definição e uso de rotas filhas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app.routing.module';
 import { CursosModule } from './cursos/cursos.module';
+import { AlunosModule } from './alunos/alunos.module';
 
 @NgModule({
   declarations: [
@@ -17,9 +18,10 @@ import { CursosModule } from './cursos/cursos.module';
   ],
   imports: [
     BrowserModule,
-    CursosModule,
     AppRoutingModule,
-    NgbModule
+    CursosModule,
+    NgbModule,
+    AlunosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
